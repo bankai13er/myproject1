9de17cdd0dd885f28671a72065b70a4a70076a6b
bankai13er <147929170+bankai13er@users.noreply.github.com>
2025-05-03 18:33:48 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+document.getElementById('redirectBtn').addEventListener('click', function() {
+    // Замените URL на тот, на который вы хотите перенаправить
+    window.location.href = 'https://www.example.com';  // Сюда вставьте нужный URL
+});
